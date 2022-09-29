0f092d2e1f0df2e8ce8a1201612b4f72dfb7947f
Juan Alvarez Cuartas <JuanAlvarezCuartas@hotmail.com>
2022-09-28 22:04:29 -0500

    Obtener el controlador desde URL

--- libs/app.php
+++ libs/app.php
@@ -4,12 +4,14 @@ class App {
 
     function __construct(){
         
-        $url = $_GET['url'];            // Obtenemos la URL
-        $url = rtrim($url , '/');       // Retirar / de la derecha
-        $url = explode('/', $url);      // separa la URL por / convirtrirndolo en Array
-
-        var_dump($url);
+        $url = $_GET['url'];                // Obtenemos la URL
+        $url = rtrim($url , '/');           // Retirar / de la derecha
+        $url = explode('/', $url);          // separa la URL por / convirtrirndolo en Array
         
+        $archivoController = 'controllers/' . $url[0] . 'Controller.php';   // Obtiene el Nombre del Controlador desde  el primer parametro de la URL
+        
+        require_once $archivoController;    // importa el Aechivo del Controlador 
+        $controller = new $url[0];          // instancia el controlador
         
     }
     
