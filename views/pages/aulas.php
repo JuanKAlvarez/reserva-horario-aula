701d542c4bdafc2a23ed0ffa5842b973d7492da5
Juan Alvarez Cuartas <JuanAlvarezCuartas@hotmail.com>
2022-11-28 00:52:28 -0500

    Modulo AULAS

--- views/pages/aulas.php
+++ views/pages/aulas.php
@@ -63,7 +63,10 @@ $campos = [ "Numero",
                                                         name="<?php echo $campo_  ?>" 
                                                         placeholder="<?php echo $campo  ?>" 
                                                         type="number" 
-                                                        class="form-control input-md" required>
+                                                        class="form-control input-md" 
+                                                        min="1" 
+                                                        max="999"
+                                                        required>
                                             </div>
                                         </div>
                                     </div>
@@ -78,11 +81,38 @@ $campos = [ "Numero",
                                                 <?php echo $campo  ?>
                                             </label>  
                                             <div class="col-md-10">
-                                                <input  id="<?php echo $campo_  ?>" 
+                                                <select   id="<?php echo $campo_  ?>" 
                                                         name="<?php echo $campo_  ?>" 
                                                         placeholder="<?php echo $campo  ?>" 
                                                         type="text" 
                                                         class="form-control input-md" required>
+                                                    <option value="A">A</option>
+                                                    <option value="B">B</option>
+                                                    <option value="C">C</option>
+                                                    <option value="D">D</option>
+                                                    <option value="E">E</option>
+                                                    <option value="F">F</option>
+                                                    <option value="G">G</option>
+                                                    <option value="H">H</option>
+                                                    <option value="I">I</option>
+                                                    <option value="J">J</option>
+                                                    <option value="k">k</option>
+                                                    <option value="L">L</option>
+                                                    <option value="M">M</option>
+                                                    <option value="N">N</option>
+                                                    <option value="O">O</option>
+                                                    <option value="P">P</option>
+                                                    <option value="Q">Q</option>
+                                                    <option value="R">R</option>
+                                                    <option value="S">S</option>
+                                                    <option value="T">T</option>
+                                                    <option value="U">U</option>
+                                                    <option value="V">V</option>
+                                                    <option value="W">W</option>
+                                                    <option value="X">X</option>
+                                                    <option value="Y">Y</option>
+                                                    <option value="Z">Z</option>
+                                                </select>
                                             </div>
                                         </div>
                                     </div>
