0b94c714b55b6c19acf9be1d57f1631cca108cdd
Juan Alvarez Cuartas <JuanAlvarezCuartas@hotmail.com>
2022-12-03 04:00:21 -0500

    Modulo Marerias - Form

--- views/pages/materias.php
+++ views/pages/materias.php
@@ -1,90 +1,140 @@
 <?php 
- $page = "Materias"; 
+require_once  $_SERVER['DOCUMENT_ROOT']."/reserva-horario-aula/libs/app.php";
+$page = "materias"; 
+$pageForm = "'$page'"; 
+$campos = [ "Código",
+            "Nombre",
+            "Duración_Horas"];
 ?>
-   
+
             <!-- Content Header (Page header) -->
             <div class="content-header">
-            <div class="container-fluid">
-                <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1 class="m-0"><?php  echo $page; ?></h1>
-                </div><!-- /.col -->
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                    <li class="breadcrumb-item"><a href="#">Home</a></li>
-                    <li class="breadcrumb-item active"><?php  echo $page; ?> </li>
-                    </ol>
-                </div><!-- /.col -->
-                </div><!-- /.row -->
-            </div><!-- /.container-fluid -->
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0"><?php  echo strtoupper($page);  ?></h1>
+                    </div><!-- /.col -->
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active"><?php  echo $page; ?> </li>
+                        </ol>
+                    </div><!-- /.col -->
+                    </div><!-- /.row -->
+                </div><!-- /.container-fluid -->
             </div>
             <!-- /.content-header -->
 
             <!-- Main content -->
             <section class="content">
                 <div class="container-fluid">
-                    <!-- Formulario de Ingreso de Informacion -->
+                    <!-- Formulario de Ingreso de Información -->
                     <div class="row">
                         <div class="col-lg-2 col-1">
                         </div>
                         <div class="col-lg-8 col-10">
                             <!-- general form elements -->
-                            <div class="card card-primary">
-                                <div class="card-header">
-                                <h3 class="card-title">Captura de <?php  echo $page; ?></h3>
+                            <div class="card card-primary ">
+                                <div class="card-header " >
+                                    <h3 class="card-title">Captura de <?php  echo ucwords($page); ?></h3>
                                 </div>
                                 <!-- /.card-header -->
                                 <!-- form start -->
-                                <form class="form-horizontal">
-                                <fieldset>
-
-                                <!-- Form Name -->
-                                <legend><?php  echo $page; ?></legend>
-
-                                <!-- Text input-->
-                                <div class="form-group">
-                                <label class="col-md-4 control-label" for="codigo">Código de la Materia </label>  
-                                <div class="col-md-4">
-                                <input id="codigo" name="codigo" type="text" placeholder="Código de la Materia " class="form-control input-md" required="">
-                                <span class="help-block">Código de la Materia </span>  
+                                <form class="form-horizontal " id="Formulario">
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <br>
+                                    </div>
                                 </div>
+                                
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <?php 
+                                            $campo = $campos[0];
+                                            $campo_ = formatCode($campo) ;
+                                        ?>
+                                        <!-- Text input <?php echo $campo  ?>-->
+                                        <div class="form-group">
+                                            <label class="control-label col-md-10" for="<?php echo $campo_  ?>">
+                                                <?php echo $campo  ?>
+                                            </label>  
+                                            <div class="col-md-10">
+                                                <input  id="<?php echo $campo_  ?>" 
+                                                        name="<?php echo $campo_  ?>" 
+                                                        placeholder="<?php echo $campo  ?>" 
+                                                        type="number" 
+                                                        class="form-control input-md" 
+                                                        min="1" 
+                                                        max="999"
+                                                        required>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <?php 
+                                            $campo = $campos[1];
+                                            $campo_ = formatCode($campo) ;
+                                        ?>
+                                        <!-- Text input <?php echo $campo  ?>-->
+                                        <div class="form-group">
+                                            <label class="control-label col-md-10" for="<?php echo $campo_  ?>">
+                                                <?php echo $campo  ?>
+                                            </label>  
+                                            <div class="col-md-10">
+                                            <input  id="<?php echo $campo_  ?>" 
+                                                        name="<?php echo $campo_  ?>" 
+                                                        placeholder="<?php echo $campo  ?>" 
+                                                        type="text" 
+                                                        class="form-control input-md" 
+                                                        required>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
-
-                                <!-- Text input-->
-                                <div class="form-group">
-                                <label class="col-md-4 control-label" for="nombre">Nombre</label>  
-                                <div class="col-md-4">
-                                <input id="nombre" name="nombre" type="text" placeholder="Nombre" class="form-control input-md" required="">
-                                <span class="help-block">Nombre</span>  
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <?php 
+                                            $campo = $campos[2];
+                                            $campo_ = formatCode($campo) ;
+                                        ?>
+                                        <!-- Text input <?php echo $campo  ?>-->
+                                        <div class="form-group">
+                                            <label class="control-label col-md-10" for="<?php echo $campo_  ?>">
+                                                Duración en Horas
+                                            </label>  
+                                            <div class="col-md-10">
+                                            <input  id="<?php echo $campo_  ?>" 
+                                                        name="<?php echo $campo_  ?>" 
+                                                        placeholder="Duración en Horas" 
+                                                        type="number" 
+                                                        class="form-control input-md" 
+                                                        min="1" 
+                                                        max="999"
+                                                        required>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        
+                                    </div>
                                 </div>
+                                <div class="row">
+                                <div class="col-md-6">
+                                        <!-- Button (Double) -->
+                                        <div class="form-group">
+                                            <label class="col-md-10 control-label" for="guardar"></label>
+                                            <div class="col-md-8">
+                                                <button id="guardar" type="button"  OnClick="saveForm(<?php  echo $pageForm; ?>)"  name="guardar" class="btn btn-primary">Guardar</button>
+                                                <button id="limpiar" name="limpiar" class="btn btn-info" type="reset">Limpiar</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                    </div>
                                 </div>
-
-                                <!-- Text input-->
-                                <div class="form-group">
-                                <label class="col-md-4 control-label" for="duracion ">Duración </label>  
-                                <div class="col-md-4">
-                                <input id="duracion " name="duracion " type="text" placeholder="Duración " class="form-control input-md" required="">
-                                <span class="help-block">Duración </span>  
-                                </div>
-                                </div>
-
-                                <!-- Button (Double) -->
-                                <div class="form-group">
-                                <label class="col-md-4 control-label" for="guardar"></label>
-                                <div class="col-md-8">
-                                    <button id="guardar" name="guardar" class="btn btn-primary">Guardar</button>
-                                    <button id="limpiar" name="limpiar" class="btn btn-info">Limpiar</button>
-                                </div>
-                                </div>
-
+                                
                                 </fieldset>
                                 </form>
-
-                                
-
-
-
-
                             </div>
                             <!-- /.card -->
                         </div>
@@ -101,11 +151,11 @@
                                 <div class="card">
                                     <div class="card-header">
                                     <h3 class="card-title">Registros</h3>
-                    
+
                                     <div class="card-tools">
                                         <div class="input-group input-group-sm" style="width: 150px;">
                                         <input type="text" name="table_search" class="form-control float-right" placeholder="Search">
-                    
+
                                         <div class="input-group-append">
                                             <button type="submit" class="btn btn-default">
                                             <i class="fas fa-search"></i>
@@ -116,82 +166,20 @@
                                     </div>
                                     <!-- /.card-header -->
                                     <div class="card-body table-responsive p-0" style="height: 300px;">
-                                    <table class="table table-head-fixed text-nowrap">
+                                    <table id="tabla" class="table table-head-fixed text-nowrap">
                                         <thead>
                                         <tr>
-                                            <th>ID</th>
-                                            <th>Tipo Gastos</th>
-                                            <th>Fecha</th>
-                                            <th>Valor</th>
-                                            <th>Descripcion</th>
-                                            <th>Estado</th>
+                                            <th>Id</th>
+                                            <?php  
+                                                foreach($campos as $campo){
+                                                    echo "<th>".$campo."</th>";
+                                                }
+                                            ?>
+                                            <th>Acciones</th>
                                         </tr>
                                         </thead>
                                         <tbody>
-                                        <tr>
-                                            <td>183</td>
-                                            <td>John Doe</td>
-                                            <td>11-7-2014</td>
-                                            <td>$50.000,00</td>
-                                            <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-                                            <td><span class="tag tag-success">Approved</span></td>
-                                        </tr>
-                                        <tr>
-                                            <td>219</td>
-                                            <td>Alexander Pierce</td>
-                                            <td>11-7-2014</td>
-                                            <td>$50.000,00</td>
-                                            <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-                                            <td><span class="tag tag-warning">Pending</span></td>
-                                        </tr>
-                                        <tr>
-                                            <td>657</td>
-                                            <td>Bob Doe</td>
-                                            <td>11-7-2014</td>
-                                            <td>$50.000,00</td>
-                                            <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-                                            <td><span class="tag tag-primary">Approved</span></td>
-                                        </tr>
-                                        <tr>
-                                            <td>175</td>
-                                            <td>Mike Doe</td>
-                                            <td>11-7-2014</td>
-                                            <td>$50.000,00</td>
-                                            <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-                                            <td><span class="tag tag-danger">Denied</span></td>
-                                        </tr>
-                                        <tr>
-                                            <td>134</td>
-                                            <td>Jim Doe</td>
-                                            <td>11-7-2014</td>
-                                            <td>$50.000,00</td>
-                                            <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-                                            <td><span class="tag tag-success">Approved</span></td>
-                                        </tr>
-                                        <tr>
-                                            <td>494</td>
-                                            <td>Victoria Doe</td>
-                                            <td>11-7-2014</td>
-                                            <td>$50.000,00</td>
-                                            <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-                                            <td><span class="tag tag-warning">Pending</span></td>
-                                        </tr>
-                                        <tr>
-                                            <td>832</td>
-                                            <td>Michael Doe</td>
-                                            <td>11-7-2014</td>
-                                            <td>$50.000,00</td>
-                                            <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-                                            <td><span class="tag tag-primary">Approved</span></td>
-                                        </tr>
-                                        <tr>
-                                            <td>982</td>
-                                            <td>Rocky Doe</td>
-                                            <td>11-7-2014</td>
-                                            <td>$50.000,00</td>
-                                            <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-                                            <td><span class="tag tag-danger">Denied</span></td>
-                                        </tr>
+                                            
                                         </tbody>
                                     </table>
                                     </div>
@@ -204,7 +192,10 @@
                         <div class="col-lg-2 col-1">
                         </div>
                     </div>     
-               
                 </div><!-- /.container-fluid -->
             </section>
             <!-- /.content Main content-->
+            <!-- Scripts de pagina-->
+            <script>
+                $('.select2').select2();
+            </script>
